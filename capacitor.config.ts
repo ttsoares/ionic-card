6caf60e4af9ef641988a9fb621934c9e16c8bcd9
Thomas T. Soares <ttsoares@orion.ufrgs.br>
2024-09-25 09:19:09 -0300

    Strucure from scratch...

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'tw-ionic',
+  appName: 'base-Ionic',
   webDir: 'dist'
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,8 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,jsx,ts,tsx,css}"],
   darkMode: "media",
   theme: {
-    extend: {
-      fontFamily: {
-        ku4: ["ku4", "sans-serif"],
-        ku7: ["ku7", "sans-serif"],
-      },
-    },
-  },
-  variants: {
     extend: {},
   },
+  variants: {},
   plugins: [],
 };
